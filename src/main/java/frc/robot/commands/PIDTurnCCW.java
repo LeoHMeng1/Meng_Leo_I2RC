73fe28cb5b0914a35d67bc8824524888d4c4938a
Sabrina Meng <sabrina.meng@gmail.com>
2024-10-19 13:58:32 -0700

    changes

--- src/main/java/frc/robot/commands/PIDTurnCCW.java
+++ src/main/java/frc/robot/commands/PIDTurnCCW.java
@@ -8,7 +8,7 @@ import frc.robot.subsystems.Drivetrain;
 public class PIDTurnCCW extends Command{
     private double setPointAngle;
     private Drivetrain dt;
-    private PIDController pid = new PIDController(0.01, 0, 0);
+    private PIDController pid = new PIDController(0.5/90, 0, 0);
     public PIDTurnCCW(Drivetrain dt, double setPointAngle){
         this.dt = dt;
         this.setPointAngle = setPointAngle;
@@ -23,22 +23,15 @@ public class PIDTurnCCW extends Command{
 
     @Override
     public void execute(){
-        double output = pid.calculate(setPointAngle);
-        if(setPointAngle >= 0){
+        double output = pid.calculate(dt.getAngle(), setPointAngle);
             dt.tankDrive(-output, output);
-        }
-        else{
-            dt.tankDrive(output, -output);
-        }
     }
-
     @Override
     public void end(boolean interrupted){
         dt.tankDrive(0,0);
     }
-
     @Override
-    public boolean isFinished() {
+    public boolean isFinished(){
         return pid.atSetpoint();
     }
 }
