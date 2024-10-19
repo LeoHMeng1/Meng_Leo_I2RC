d2c8acea6a0a0cdefe155b3ff3c42bf8b15faf1f
Sabrina Meng <sabrina.meng@gmail.com>
2024-10-19 14:58:43 -0700

    Setting up for whatever happens next

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,6 +9,7 @@ import frc.robot.commands.PIDTurnCCW;
 import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
@@ -53,6 +54,6 @@ public class RobotContainer {
    */
     public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return pid;
+    return new SequentialCommandGroup();
   }
 }
